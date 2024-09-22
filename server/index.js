50975e62f452bca46948d85b9d9a50d539892e00
maheshshinde2002 <121389301+maheshshinde2002@users.noreply.github.com>
2024-09-22 07:18:55 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,17 +1,20 @@
-import  express from "express";
-import router from "./routes/routes.js";
+import express from 'express';
 import cors from 'cors';
-import DBConnection from "./database/db.js";
+import router from './routes/routes.js';
+import DBConnection from './database/db.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use('/', router);
 
-const PORT = 8000;
-
-
+const PORT = process.env.PORT || 5000;
 
 DBConnection();
 
-app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
